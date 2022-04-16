151530487e3ca70eccd10c9a225e5dab5129611b
AlexBurkh <alex.burkh@yandex.ru>
2022-04-16 14:14:39 +0300

    Добавлен класс вывода на консоль и отредактиврованы классы страниц

---
+++ FMCore/Engine/ConsoleDrawing/ConsoleDrawer.cs
@@ -0,0 +1,32 @@
+﻿using FMCore.Interfaces;
+using System;
+
+namespace FMCore.Engine.ConsoleDrawing
+{
+    public class ConsoleDrawer
+    {
+        ConsoleDrawer(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        ILogger logger;
+
+
+        public void DrawColoredAt(string text, (uint x, uint y) coordinates, (ConsoleColor backgroud, ConsoleColor foreground) colors)
+        {
+            Console.BackgroundColor = colors.backgroud;
+            Console.ForegroundColor = colors.foreground;
+
+            DrawAt(text, coordinates);
+
+            Console.ResetColor();
+        }
+
+        public void DrawAt(string text, (uint x, uint y) coordinates)
+        {
+            Console.SetCursorPosition((int)coordinates.x, (int)coordinates.y);
+            Console.Write(text);
+        }
+    }
+}
---
+++ FMCore/Models/Pages/ContentPage.cs
@@ -0,0 +1,61 @@
+﻿using FMCore.Interfaces;
+using FMCore.Models.Borders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMCore.Models.Pages
+{
+    public class ContentPage : Page
+    {
+        #region Конструкторы
+        public ContentPage(ILogger logger, uint height, uint width)
+            : base(logger, height, width)
+        {
+
+        }
+        #endregion
+
+
+        #region Свойства
+        Border[] Borders { get; set; }
+        Dictionary<long, FileSystemInfo> Content { get; set; }
+        #endregion
+
+
+        #region Методы
+        public override void Print()
+        {
+            PrintBorders();
+            PrintContent();
+        }
+        public void UploadContent(Dictionary<long, FileSystemInfo> content)
+        {
+            if (content is null) { return; }
+            Content = content;
+        }
+
+        private void PrintBorders()
+        {
+            for (int i = 0; i < Borders.Length; i++)
+            {
+                var border = Borders[i];
+                border.Draw();
+            }
+        }
+        private void PrintContent()
+        {
+            for (int i = 0; i < Content.Count; i++)
+            {
+                if (Content.TryGetValue(i, out var content))
+                {
+                    /// printing "ContentString"
+                }
+            }
+        }
+        #endregion
+    }
+}
